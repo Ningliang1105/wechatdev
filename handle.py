59533da59af2443a8bbae82c3f0a295324a10631
Ningliang1105 <ningliang.j@gmail.com>
2017-06-19 03:18:07 +0800

    bug

--- handle.py
+++ handle.py
@@ -62,6 +62,7 @@ class Handle(object):
                         query = {'cityname': cityname, 'key': '59c4d4057feed1a7ac32e7055ae7d849', 'dtype':'json'}
                         resp = requests.get(url, params=query)
                         weather = resp.json()['result']['today']['weather']
+                        print weather
                         #dressing = resp.json()['result']['today']['dressing_advice']
                         #message = weather + dressing
                         replyMsg = reply.TextMsg(toUser, fromUser, weather)
