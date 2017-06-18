c2652a7a206142c247519a673c414f962b0a665c
Ningliang1105 <ningliang.j@gmail.com>
2017-06-18 15:29:07 +0800

    bug

--- handle.py
+++ handle.py
@@ -5,11 +5,48 @@ import hashlib
 import reply
 import receive
 import web
-import imgtest
 import os
 import time
 import lxml
 import hashlib
+import requests
+import re
+
+def imgtest(picurl):
+    print "Hello world"
+    s = requests.session()
+    url = 'http://how-old.net/Home/Analyze?isTest=False&source=&version=001'
+    header = {
+    'Accept-Encoding':'gzip, deflate',
+    'User-Agent': "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:34.0) Gecko/20100101 Firefox/34.0",
+    'Host': "how-old.net",
+    'Referer': "http://how-old.net/",
+    'X-Requested-With': "XMLHttpRequest"
+        }
+    print "OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO"
+    data = {'file':s.get(picurl).content}
+    #data = {'file': open(sid+'.jpg', 'rb')}
+    #此处打开指定的jpg文件
+
+    print "HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH2"
+
+
+    r = s.post(url, files=data, headers=header)
+    h = r.content
+    i = h.replace('\\','')
+    #j = eval(i)
+    print i
+
+    gender = re.search(r'"gender": "(.*?)"rn', i)
+    age = re.search(r'"age": (.*?),rn', i)
+    if gender.group(1) == 'Male':
+        gender1 = '男'
+    else:
+        gender1 = '女'
+    #print gender1
+    #print age.group(1)
+    datas = [gender1, age.group(1)]
+    return datas
 
 class Handle(object):
 
