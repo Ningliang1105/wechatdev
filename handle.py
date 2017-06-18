aa8ddc49b79aac465ce7b514bb7e0e059e3036eb
Ningliang1105 <ningliang.j@gmail.com>
2017-06-19 03:28:27 +0800

    bug

--- handle.py
+++ handle.py
@@ -61,11 +61,11 @@ class Handle(object):
                         url = 'http://v.juhe.cn/weather/index'
                         query = {'cityname': cityname, 'key': '59c4d4057feed1a7ac32e7055ae7d849', 'dtype':'json'}
                         resp = requests.get(url, params=query)
-                        weather = resp.json()['result']['today']
+                        weather = resp.json()['result']['today']['weather']
                         print weather
                         #dressing = resp.json()['result']['today']['dressing_advice']
                         #message = weather + dressing
-                        replyMsg = reply.TextMsg(toUser, fromUser, weather)
+                        replyMsg = reply.TextMsg(toUser, fromUser, str(weather))
                     else:
                         replyMsg = reply.TextMsg(toUser, fromUser, 'test')
                     return replyMsg.send()
