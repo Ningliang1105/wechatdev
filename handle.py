00e25992fcd3204fdac0ad3114426af82670bb45
Ningliang1105 <ningliang.j@gmail.com>
2017-06-18 15:40:49 +0800

    bug

--- handle.py
+++ handle.py
@@ -66,8 +66,8 @@ class Handle(object):
                 BASE_URL = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0'  # Replace with your regional Base URL
                 CF.BaseUrl.set(BASE_URL)
 
-                #img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
-                result = CF.face.detect(picurl)
+                img_url = 'https://raw.githubusercontent.com/Microsoft/Cognitive-Face-Windows/master/Data/detection1.jpg'
+                result = CF.face.detect(img_url)
                 print result
                 #content = '图中人物性别为'+datas[0]+'\n'+'年龄为'+datas[1]
                 #replyMsg = reply.TextMsg(toUser, fromUser, content)
